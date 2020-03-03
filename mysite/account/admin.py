c1cbcfc2a38f00fd0d4710bbfb2f3aab4a489c82
JeongtaekLim <ljt1100@gmail.com>
2020-03-03 18:19:46 +0900

    custom user생성완료

--- mysite/account/admin.py
+++ mysite/account/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = (
+        'nickname',
+        'email',
+        'date_joined',
+    )
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('nickname',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'password1', 'password2')}
+         ),
+    )
+    ordering = ()  # Contain only fields in your `custom-user-model` intended to ordering
+    filter_horizontal = ()  # Leave it empty. You have neither `groups` or `user_permissions`
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
--- mysite/account/models.py
+++ mysite/account/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    user_in_migrations = True
+
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('must have user email')
+        user = self.model(email=self.normalize_email(email), nickname=nickname)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+        user = self.create_user(
+            email,
+            password=password,
+            nickname=nickname
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    objects = UserManager()
+    email = models.EmailField(max_length=255, unique=True)
+    nickname = models.CharField(max_length=20, null=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
